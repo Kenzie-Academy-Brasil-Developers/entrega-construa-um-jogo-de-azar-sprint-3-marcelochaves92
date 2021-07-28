209c71d603fa3c564c3b05d99b672c93f2811edb
Marcelo Chaves <9marceli@gmail.com>
2021-07-27 22:12:57 -0300

    Mudanças no Pedra, Papel e Tesoura

--- script.js
+++ script.js
@@ -1,71 +1,60 @@
-let gameImg = document.getElementsByClassName('gameimg')
+let jogadorEscolha = 0;
+let computadorEscolha = 0;
+let ganhador = -1;
+let jogadorPontuacao = 0;
+let computadorPontuacao = 0;
+function jogar(escolha){
+    jogadorEscolha = escolha;
 
-let playerPick = 0;
-let playerScore = 0;
-let robotPick = 0;
-let robotScore = 0;
-let winner = -1; 
+    computadorEscolha = Math.floor((Math.randon() * (3-1 + 1))) + 1;
 
-// 1 - Pedra
-// 2 - Papel
-// 3 - Tesoura
-let gameLogic = function(playerOption, robotOption) {
-    if((playerOption == 1 && robotOption == 1) || (playerOption == 2 && robotOption == 2) || (playerOption == 3 && robotOption == 3)) {
-        winner = 0
-    } else if(playerOption == 1 && robotOption == 3) {
-        winner = 1
-    } else if(playerOption == 2 && robotOption == 1) {
-        winner = 1
-    } else if(playerOption == 3 && robotOption == 2) {
-        winner = 1
-    } else {
-        winner = 2
+    // 1- Pedra
+    // 2- Papel
+    // 3- Tesoura
+
+    if ((jogadorEscolha) === 1 && (computadorEscolha === 1)){
+        ganhador = 0;
     }
-    return winner;
-}
-
-let whoWins = function(winner) {
-    let resultMessage = document.getElementById('messageResult')
-    if(winner === 0) {
-        resultMessage.innerHTML = 'EMPATE';
-    } else if(winner === 1) {
-        resultMessage.innerHTML = 'Humano WINS !!!';
-        playerScore++;
-        document.getElementById('player-score').innerHTML = `Score: ${playerScore}`
-    } else if (winner === 2) {
-        resultMessage.innerHTML = 'Computador WINS !!!';
-        robotScore++;
-        document.getElementById('robot-score').innerHTML = `Score: ${robotScore}`
+    else if((jogadorEscolha) === 1 && (computadorEscolha === 2)){
+        ganhador = 2;
     }
-}
+    else if((jogadorEscolha) === 1 && (computadorEscolha === 3)){
+        ganhador = 1;
+    }
+    else if((jogadorEscolha) === 2 && (computadorEscolha === 1)){
+        ganhador = 1;
+    }
+    else if((jogadorEscolha) === 2 && (computadorEscolha === 2)){
+        ganhador = 0;
+    }
+    else if((jogadorEscolha) === 2 && (computadorEscolha === 3)){
+        ganhador = 2;
+    }
+    else if((jogadorEscolha) === 3 && (computadorEscolha === 1)){
+        ganhador = 2;
+    }
+    else if((jogadorEscolha) === 3 && (computadorEscolha === 2)){
+        ganhador = 1;
+    }
+    else if((jogadorEscolha) === 3 && (computadorEscolha === 3)){
+        ganhador = 0;
+    }
+    document.getElementById("jogador-escolhar-" + jogadorEscolha).classList.remove('Selecionado');
+    document.getElementById("computador-escolha-" + computadorEscolha).classList.remove('Selecionado');
+    document.getElementById("jogador-escolha-" + jogadorEscolha).classList.add('Selecionado');
+    document.getElementById("computador-escolha-" + computadorEscolha).classList.add('Selecionado');
 
-for(let i = 0; i < gameImg.length; i++) {
-
-    gameImg[i].style.opacity = '0.2'
-    
-    gameImg[i].addEventListener('click', function(element) {
-        let tempId = element.target.parentNode.id;
-        let tempPick = tempId.split('-')[0];
-        
-        if (tempPick === 'player') {
-            for(let j = 0; j < gameImg.length; j++) {
-                gameImg[j].style.opacity = '0.2';
-            }
-
-            playerPick = tempId.split('-')[2];
-            document.getElementById('player-pick-' + playerPick).firstChild.style.opacity = '1.0'
-
-            robotPick = Math.floor((Math.random() * 3) + 1)
-            document.getElementById('robot-pick-' + robotPick).firstChild.style.opacity = '1.0'
-
-            console.log('Escolha do humano', playerPick);
-            console.log('Escolha do computador', robotPick);
-            
-            whoWins(gameLogic(playerPick, robotPick));
-            console.log('-----------------------------');
-        } else {
-            // Player is not able to select from the Robot's side
-            alert('Você não é o robô');
-        }
-    })
+    if(ganhador === 0){
+    document.getElementById("mensagens").innerHTML = "Empate";
+    }
+    else if(ganhador === 1){
+        document.getElementById("mensagens").innerHTML = "Humano ganhou";
+        jogadorPontuacao++
+    }
+    else if(ganhador === 2){
+        document.getElementById("mensagens").innerHTML = "Computador ganhou";
+        computadorPontuacao++
+    }
+    document.getElementById("jogador-pontos").innerHTML = jogadorPontuacao;
+    document.getElementById("computador-pontos").innerHTML = computadorPontuacao;
 }
